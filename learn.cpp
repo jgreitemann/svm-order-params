5306b3b0e4fe227cc58950891782ef0a9457f149
Jonas Greitemann <j.greitemann@lmu.de>
2017-11-20 14:56:03 +0100

    Parallelized the MC samples in the learn program

--- learn.cpp
+++ learn.cpp
@@ -9,6 +9,10 @@
 #include "test_adapter.hpp"
 
 #include <iostream>
+#include <memory>
+#include <utility>
+
+#include <omp.h>
 
 #include <alps/accumulators.hpp>
 #include <alps/mc/api.hpp>
@@ -28,10 +32,12 @@
 #endif
 
 
+using sim_type = training_adapter<sim_base>;
+using kernel_t = typename sim_type::kernel_t;
+using model_t = svm::model<kernel_t>;
+
 int main(int argc, char** argv)
 {
-    typedef training_adapter<sim_base> sim_type;
-
     try {
     
         // Creates the parameters for the simulation
@@ -50,50 +56,72 @@ int main(int argc, char** argv)
             parameters.has_missing(std::cout)) {
             return 1;
         }
-    
-        std::cout << "Creating simulation" << std::endl;
-        sim_type sim(parameters); 
 
-        // If needed, restore the last checkpoint
+        svm::problem<kernel_t> prob(0);
         std::string checkpoint_file = parameters["checkpoint"].as<std::string>();
-        
-        if (parameters.is_restored()) {
-            std::cout << "Restoring checkpoint from " << checkpoint_file
-                      << std::endl;
-            sim.load(checkpoint_file);
+    
+#pragma omp parallel
+        {
+            int nthread = omp_get_num_threads();
+            int tid = omp_get_thread_num();
+
+            // reduce number of temps to be sampled
+            alps::params local_params(parameters);
+            if (!local_params.is_restored()) {
+                local_params["N_temp"] = local_params["N_temp"].as<size_t>() / nthread;
+            }
+
+            std::unique_ptr<sim_type> sim;
+#pragma omp critical
+            sim = std::unique_ptr<sim_type>(new sim_type(local_params, tid));
+
+            std::string checkpoint_path = [&] {
+                std::stringstream ss;
+                ss << "simulation/clones/" << tid;
+                return ss.str();
+            } ();
+
+            // If needed, restore the last checkpoint
+#pragma omp critical
+            if (local_params.is_restored()) {
+                std::cout << "Restoring checkpoint from " << checkpoint_file
+                          << " (thread " << tid << ")"
+                          << std::endl;
+                alps::hdf5::archive cp(checkpoint_file, "r");
+                cp[checkpoint_path] >> *sim;
+            }
+
+            sim->run(alps::stop_callback(size_t(local_params["timelimit"])));
+
+            // Checkpoint the simulation
+#pragma omp critical
+            {
+                std::cout << "Checkpointing simulation to " << checkpoint_file
+                          << " (thread " << tid << ")"
+                          << std::endl;
+                alps::hdf5::archive cp(checkpoint_file, "w");
+                cp[checkpoint_path] << *sim;
+            }
+
+            if (tid == 0) {
+                prob = sim->surrender_problem();
+            }
+#pragma omp barrier
+            if (tid != 0) {
+#pragma omp critical
+                prob.append_problem(sim->surrender_problem());
+            }
         }
 
-        // Run the simulation
-        std::cout << "Running simulation" << std::endl;
-        sim.run(alps::stop_callback(size_t(parameters["timelimit"])));
-
-        // Checkpoint the simulation
-        std::cout << "Checkpointing simulation to " << checkpoint_file
-                  << std::endl;
-        sim.save(checkpoint_file);
-
-        alps::results_type<sim_type>::type results = alps::collect_results(sim);
-
-        // Print results
         {
-            using alps::accumulators::result_wrapper;
-            std::cout << "All measured results:" << std::endl;
-            std::cout << results << std::endl;
-            
-            std::cout << "Simulation ran for "
-                      << results["Energy"].count()
-                      << " steps." << std::endl;
-
             // create the model
-            using kernel_t = typename sim_type::kernel_t;
-            using model_t = svm::model<kernel_t>;
             svm::parameters<kernel_t> kernel_params(1., 0.,
                                                     parameters["C"].as<double>(),
                                                     svm::machine_type::C_SVC);
             std::cout << "Creating SVM model..."
                       << " (C = " << parameters["C"].as<double>() << ')'
                       << std::endl;
-            model_t model(sim.surrender_problem(), kernel_params);
+            model_t model(std::move(prob), kernel_params);
 
             // set up serializer
             svm::model_serializer<svm::hdf5_tag, model_t> serial(model);
@@ -102,7 +130,6 @@ int main(int argc, char** argv)
             std::string output_file = parameters["outputfile"];
             alps::hdf5::archive ar(output_file, "w");
             ar["/parameters"] << parameters;
-            ar["/simulation/results"] << results;
             ar["/model"] << serial;
         }
         return 0;
