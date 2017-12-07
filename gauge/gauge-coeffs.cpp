8ac86869bd9bd533d7adabdb5c1135087224a4f9
Jonas Greitemann <j.greitemann@lmu.de>
2017-12-07 15:18:11 +0100

    Generate PPM images of coeff matrix and block structure

--- gauge/gauge-coeffs.cpp
+++ gauge/gauge-coeffs.cpp
@@ -1,9 +1,12 @@
 #include "gauge.hpp"
 #include "svm-wrapper.hpp"
 #include "hdf5_serialization.hpp"
+#include "colormap.hpp"
 
 #include <array>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include <alps/hdf5.hpp>
 #include <alps/utilities/fs/remove_extensions.hpp>
@@ -13,6 +16,48 @@
 
 using sim_type = gauge_sim;
 using kernel_t = svm::kernel::polynomial<2>;
+
+void normalize_matrix (boost::multi_array<double,2> & mat) {
+    double absmax = 0.;
+    for (size_t i = 0; i < mat.shape()[0]; ++i)
+        for (size_t j = 0; j < mat.shape()[0]; ++j)
+            if (absmax < std::abs(mat[i][j]))
+                absmax = std::abs(mat[i][j]);
+    absmax = 1./absmax;
+    for (size_t i = 0; i < mat.shape()[0]; ++i)
+        for (size_t j = 0; j < mat.shape()[0]; ++j)
+            mat[i][j] *= absmax;
+}
+
+template <typename Palette>
+void write_matrix (boost::multi_array<double,2> const& mat, std::string basename,
+                   Palette const& pal) {
+    /* PPM output */ {
+        std::ofstream ppm(basename + ".ppm", std::ios::binary);
+        std::string header = [&] {
+            std::stringstream ss;
+            ss << "P6\n" << mat.shape()[0] << ' ' << mat.shape()[1] << '\n'
+            << size_t(Palette::color_type::depth()) << '\n';
+            return ss.str();
+        } ();
+        ppm.write(header.c_str(), header.size());
+        for (int i = mat.shape()[0] - 1; i >= 0; --i) {
+            for (size_t j = 0; j < mat.shape()[1]; ++j) {
+                auto pix = pal(mat[i][j]);
+                pix.write(ppm);
+            }
+        }
+    }
+    /* TXT output */ {
+        std::ofstream txt(basename + ".txt");
+        for (size_t i = 0; i < mat.shape()[0]; ++i) {
+            for (size_t j = 0; j < mat.shape()[1]; ++j) {
+                txt << mat[i][j] << '\t';
+            }
+            txt << '\n';
+        }
+    }
+}
 
 int main(int argc, char** argv) {
     try {
@@ -43,32 +88,22 @@ int main(int argc, char** argv) {
             }
         }
 
-        // rearrange elements in blocks by components
         std::unique_ptr<config_policy> confpol =
             sim_type::config_policy_from_parameters(parameters);
         {
             auto rearranged_coeffs = confpol->rearrange_by_component(coeffs);
-            std::string outname = alps::fs::remove_extensions(arname) + ".coeffs.txt";
-            std::ofstream os(outname);
-            for (size_t i = 0; i < rearranged_coeffs.shape()[0]; ++i) {
-                for (size_t j = 0; j < rearranged_coeffs.shape()[1]; ++j) {
-                    os << rearranged_coeffs[i][j] << '\t';
-                }
-                os << '\n';
-            }
+            normalize_matrix(rearranged_coeffs);
+            write_matrix(rearranged_coeffs,
+                         alps::fs::remove_extensions(arname) + ".coeffs",
+                         color::palettes.at("rdbu").rescale(-1, 1));
         }
         {
             auto block_structure = confpol->block_structure(coeffs);
-            std::string outname = alps::fs::remove_extensions(arname) + ".blocks.txt";
-            std::ofstream os(outname);
-            for (size_t i = 0; i < block_structure.shape()[0]; ++i) {
-                for (size_t j = 0; j < block_structure.shape()[1]; ++j) {
-                    os << block_structure[i][j] << '\t';
-                }
-                os << '\n';
-            }
+            normalize_matrix(block_structure);
+            write_matrix(block_structure,
+                         alps::fs::remove_extensions(arname) + ".blocks",
+                         color::palettes.at("parula"));
         }
-
         return 0;
     } catch (const std::exception& exc) {
         std::cout << "Exception caught: " << exc.what() << std::endl;
