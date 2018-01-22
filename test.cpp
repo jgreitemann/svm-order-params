5e329e1a36604d104f7ed40a9a5ebd3c8a592940
Jonas Greitemann <j.greitemann@lmu.de>
2018-01-22 23:38:43 +0100

    Removed critical construct around sim_type ctor
    In old ALPSCore versions, there was a race condition in the default ctor of
    accumulator_set which meant that the mcbase ctor was not thread-safe. This has
    been fixed upstream, such that the awkward workaround (heap alloc of sim class)
    is no longer necessary.

--- test.cpp
+++ test.cpp
@@ -111,12 +111,10 @@ int main(int argc, char** argv)
                 alps::params local_params(parameters);
                 local_params["temperature"] = temps[i];
 
-                std::unique_ptr<sim_type> sim;
-#pragma omp critical
-                sim = std::unique_ptr<sim_type>(new sim_type(local_params));
-                sim->run(alps::stop_callback(size_t(parameters["timelimit"])));
+                sim_type sim(local_params);
+                sim.run(alps::stop_callback(size_t(parameters["timelimit"])));
 
-                alps::results_type<sim_type>::type results = alps::collect_results(*sim);
+                alps::results_type<sim_type>::type results = alps::collect_results(sim);
                 std::stringstream ss;
 #pragma omp critical
                 {
