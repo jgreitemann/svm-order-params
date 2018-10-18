ec1a3b762950a580e31f6b099dc2a7a01afa6710
Jonas Greitemann <j.greitemann@lmu.de>
2018-10-18 15:38:31 +0200

    Move phase_space::point ns to its own header

--- phase_space_policy.hpp
+++ phase_space_policy.hpp
@@ -16,17 +16,20 @@
 
 #pragma once
 #include "label.hpp"
+#include "phase_space_point.hpp"
 #include "polygon.hpp"
 
 #include <algorithm>
 #include <cmath>
 #include <functional>
+#include <iostream>
 #include <limits>
 #include <map>
 #include <numeric>
 #include <random>
 #include <sstream>
 #include <stdexcept>
+#include <string>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -85,105 +88,6 @@ namespace phase_space {
         };
 
     };
-
-    namespace point {
-
-        struct temperature {
-            static const size_t label_dim = 1;
-            using iterator = double *;
-            using const_iterator = double const *;
-
-            static void define_parameters(alps::params & params, std::string prefix="") {
-                params.define<double>(prefix + "temp", 1., "temperature");
-            }
-
-            static bool supplied(alps::params const& params, std::string prefix="") {
-                return params.supplied(prefix + "temp");
-            }
-
-            temperature() : temp(-1) {}
-            temperature(double temp) : temp(temp) {}
-            temperature(alps::params const& params, std::string prefix="")
-                : temp(params[prefix + "temp"].as<double>()) {}
-
-            template <class Iterator>
-            temperature(Iterator begin) : temp(*begin) {}
-
-            const_iterator begin() const { return &temp; }
-            iterator begin() { return &temp; }
-            const_iterator end() const { return &temp + 1; }
-            iterator end() { return &temp + 1; }
-
-            double temp;
-        };
-
-        struct J1J3 {
-            static const size_t label_dim = 2;
-            using iterator = double *;
-            using const_iterator = double const *;
-
-            static void define_parameters(alps::params & params, std::string prefix="") {
-                params
-                    .define<double>(prefix + "J1", 0., "J1 coupling")
-                    .define<double>(prefix + "J3", 0., "J3 coupling");
-            }
-
-            static bool supplied(alps::params const& params, std::string prefix="") {
-                return params.supplied(prefix + "J1")
-                    && params.supplied(prefix + "J3");
-            }
-
-            J1J3(alps::params const& params, std::string prefix="")
-                : J{params[prefix + "J1"].as<double>(),
-                    params[prefix + "J3"].as<double>()} {}
-
-            J1J3() : J{-1, -1} {}
-            J1J3(double J1, double J3) : J{J1, J3} {}
-
-            template <class Iterator>
-            J1J3(Iterator begin) : J {*begin, *(++begin)} {}
-
-            const_iterator begin() const { return J; }
-            iterator begin() { return J; }
-            const_iterator end() const { return J + 2; }
-            iterator end() { return J + 2; }
-
-            double const& J1() const { return J[0]; }
-            double & J1() { return J[0]; }
-            double const& J3() const { return J[1]; }
-            double & J3() { return J[1]; }
-
-            double J[2];
-        };
-
-        template <typename Point>
-        bool operator== (Point const& lhs, Point const& rhs) {
-            return std::equal(lhs.begin(), lhs.end(), rhs.begin());
-        }
-
-        template <typename Point, typename = typename std::enable_if_t<(Point::label_dim > 0)>>
-        std::ostream& operator<< (std::ostream & os, Point const& p) {
-            auto it = p.begin();
-            os << '(' << *(it++);
-            for (; it != p.end(); ++it)
-                os << ", " << *it;
-            return os << ')';
-        }
-
-        template <typename Point>
-        struct distance {
-            double operator() (Point const& lhs, Point const& rhs) const {
-                double d = 0.;
-                auto itl = lhs.begin();
-                auto itr = rhs.begin();
-                for (; itl != lhs.end(); ++itl, ++itr) {
-                    d += pow(*itl - *itr, 2);
-                }
-                return sqrt(d);
-            }
-        };
-
-    }
 
     namespace classifier {
 
