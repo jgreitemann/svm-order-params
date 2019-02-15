jgreitemann/svm-order-params

commit 4a5a513974234b5fe836546a6f04c5999010499e

Made one rearranged_index function in monomial_c_p
Previously all ElementPolicies had to provide their version of
`rearranged_index`. This was unnecessary and led to bugs in the past.
DRY

config_policy.hpp
@@ -44,16 +44,6 @@ namespace element_policy {
         const size_t range() const { return N * n_block(); }
         const size_t block(size_t index) const { return 0; }
         const size_t component(size_t index) const { return index; }
-
-        size_t rearranged_index (indices_t const& ind) const {
-            size_t components = 0;
-            for (auto it = ind.begin(); it != ind.end(); ++it) {
-                components *= 3;
-                components += component(*it);
-            }
-            return components;
-        }
-
     };
 
 }
@@ -229,11 +219,11 @@ struct monomial_config_policy
         indices_t i_ind(rank_);
         for (size_t i = 0; i < size(); ++i, advance_ind(i_ind)) {
             do {
-                size_t i_out = ElementPolicy::rearranged_index(i_ind);
+                size_t i_out = rearranged_index(i_ind);
                 indices_t j_ind(rank_);
                 for (size_t j = 0; j < size(); ++j, advance_ind(j_ind)) {
                     do {
-                        size_t j_out = ElementPolicy::rearranged_index(j_ind);
+                        size_t j_out = rearranged_index(j_ind);
                         out[i_out][j_out] = c[i][j] / (weights_[i] * weights_[j]);
                     } while (unsymmetrize && transform_ind(j_ind));
                 }
@@ -256,7 +246,7 @@ struct monomial_config_policy
         for (size_t i = 0; i < size(); ++i, advance_ind(ind)) {
             do {
                 indices_t ind_block = block_indices(ind);
-                size_t out = ElementPolicy::rearranged_index(component_indices(ind));
+                size_t out = rearranged_index(component_indices(ind));
                 if (std::equal(bi.begin(), bi.end(), ind_block.begin()))
                     i_ind_lookup[out] = i;
                 if (std::equal(bj.begin(), bj.end(), ind_block.begin()))
@@ -289,11 +279,11 @@ struct monomial_config_policy
         indices_t i_ind(rank_);
         for (size_t i = 0; i < size(); ++i, advance_ind(i_ind)) {
             do {
-                size_t i_out = ElementPolicy::rearranged_index(i_ind);
+                size_t i_out = rearranged_index(i_ind);
                 indices_t j_ind(rank_);
                 for (size_t j = 0; j < size(); ++j, advance_ind(j_ind)) {
                     do {
-                        size_t j_out = ElementPolicy::rearranged_index(j_ind);
+                        size_t j_out = rearranged_index(j_ind);
                         block_2norms[i_out / block_size][j_out / block_size]
                             += c[i][j] / (weights_[i] * weights_[j]);
                         block_sums[i_out / block_size][j_out / block_size]
@@ -362,6 +352,21 @@ protected:
     using ElementPolicy::component;
 
 private:
+    size_t rearranged_index (indices_t const& ind) const {
+        size_t n_component = ElementPolicy::range() / ElementPolicy::n_block();
+        size_t components = 0;
+        size_t blocks = 0;
+        size_t shift = 1;
+        for (auto it = ind.begin(); it != ind.end(); ++it) {
+            components *= n_component;
+            components += component(*it);
+            blocks *= n_component;
+            blocks += block(*it);
+            shift *= n_component;
+        }
+        return blocks * shift + components;
+    }
+
     using SymmetryPolicy::transform_ind;
 
     size_t rank_;

frustmag/frustmag_config_policy.hpp
@@ -43,24 +43,6 @@ namespace element_policy {
         const size_t component(size_t index) const {
             return index % Lattice::value_type::size;
         }
-
-        size_t rearranged_index (indices_t const& ind) const {
-            size_t components = 0;
-            size_t blocks = 0;
-            size_t shift = 1;
-            for (auto it = ind.begin(); it != ind.end(); ++it) {
-                components *= 3;
-                components += component(*it);
-                blocks *= 3;
-                blocks += block(*it);
-                shift *= 3;
-            }
-            return blocks * shift + components;
-            // return std::accumulate(ind.begin(), ind.end(), 0,
-            //                        [] (size_t a, size_t b) {
-            //                            return Lattice::value_type::size * a + b;
-            //                        });
-        }
     };
 }
 

gauge/gauge_config_policy.hpp
@@ -36,15 +36,6 @@ namespace element_policy {
         const size_t n_color() const { return 1; }
         const size_t sublattice(size_t index) const { return 0; }
         const size_t color(size_t index) const { return 2; }
-
-        size_t rearranged_index (indices_t const& ind) const {
-            size_t components = 0;
-            for (auto it = ind.begin(); it != ind.end(); ++it) {
-                components *= 3;
-                components += component(*it);
-            }
-            return components;
-        }
     };
 
     struct triad {
@@ -55,20 +46,6 @@ namespace element_policy {
         const size_t color(size_t index) const { return index / 3; }
         const size_t block(size_t index) const { return color(index); }
         const size_t component(size_t index) const { return index % 3; }
-
-        size_t rearranged_index (indices_t const& ind) const {
-            size_t components = 0;
-            size_t colors = 0;
-            size_t shift = 1;
-            for (auto it = ind.begin(); it != ind.end(); ++it) {
-                components *= 3;
-                components += component(*it);
-                colors *= 3;
-                colors += color(*it);
-                shift *= 3;
-            }
-            return colors * shift + components;
-        }
     };
 
     template <typename BaseElementPolicy>
@@ -95,20 +72,6 @@ namespace element_policy {
         }
         const size_t component(size_t index) const {
             return BaseElementPolicy::component(index % BaseElementPolicy::range());
-        }
-
-        size_t rearranged_index (indices_t const& ind) const {
-            size_t sublats = 0;
-            size_t shift = 1;
-            for (auto it = ind.begin(); it != ind.end(); ++it) {
-                sublats *= n_unitcell();
-                sublats += sublattice(*it);
-                shift *= BaseElementPolicy::range();
-            }
-            indices_t base_ind(ind);
-            for (size_t & i : base_ind)
-                i = i % BaseElementPolicy::range();
-            return sublats * shift + BaseElementPolicy::rearranged_index(base_ind);
         }
 
     private:

ising/ising_config_policy.hpp
@@ -38,11 +38,6 @@ namespace element_policy {
         const size_t block(size_t index) const { return index; }
         const size_t component(size_t index) const { return 0; }
 
-        size_t rearranged_index (indices_t const& ind) const {
-            return std::accumulate(ind.begin(), ind.end(), 0,
-                                   [&] (size_t a, size_t b) { return L*a + b; });
-        }
-
     private:
         size_t L;
     };

commit 8f742be00ea1af6dd5738a04935294803a141b7f

Fix bug: wrong shift in rearranged_index
This bug was introduced in 4a5a5139. The "blocks" index needs to be
shifted by n_block, not n_component. This does not matter when both are
equal and was missed when testing against the gauge client code. It does
matter for the pyrochlore's tetrahedral unit cell.

config_policy.hpp
@@ -353,14 +353,15 @@ protected:
 
 private:
     size_t rearranged_index (indices_t const& ind) const {
-        size_t n_component = ElementPolicy::range() / ElementPolicy::n_block();
+        size_t n_block = ElementPolicy::n_block();
+        size_t n_component = ElementPolicy::range() / n_block;
         size_t components = 0;
         size_t blocks = 0;
         size_t shift = 1;
         for (auto it = ind.begin(); it != ind.end(); ++it) {
             components *= n_component;
             components += component(*it);
-            blocks *= n_component;
+            blocks *= n_block;
             blocks += block(*it);
             shift *= n_component;
         }