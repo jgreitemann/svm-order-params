5f244c8046189e8d599a65e49fe065b2e08012e1
Jonas Greitemann <j.greitemann@lmu.de>
2019-06-19 18:42:56 +0200

    Select "cluster" ("lattice" / "single") parameter
    Users can choose to use the lattice unit cell ("lattice"; previous
    behavior; default) or single spins ("single") as the "cluster"
    parameter. This mirrors the similar feature supported by the gauge model
    config policy for a long time.

--- frustmag/frustmag_config_policy.hpp
+++ frustmag/frustmag_config_policy.hpp
@@ -44,14 +44,28 @@ namespace element_policy {
             return index % Lattice::value_type::size;
         }
     };
+
+    template <typename Lattice>
+    struct single {
+        constexpr size_t n_block() const { return 1; }
+        constexpr size_t range() const {
+            return Lattice::value_type::size;
+        }
+        constexpr size_t block(size_t index) const {
+            return 0;
+        }
+        constexpr size_t component(size_t index) const {
+            return index % Lattice::value_type::size;
+        }
+    };
 }
 
-template <typename Config, typename Introspector, typename SymmetryPolicy>
+template <typename Config, typename Introspector, typename SymmetryPolicy,
+          typename ElementPolicy>
 struct frustmag_config_policy
     : public monomial_config_policy<Config, Introspector, SymmetryPolicy,
-                                    element_policy::lattice<Config>>
+                                    ElementPolicy>
 {
-    using ElementPolicy = element_policy::lattice<Config>;
     using BasePolicy = monomial_config_policy<Config, Introspector,
                                               SymmetryPolicy, ElementPolicy>;
     using config_array = typename BasePolicy::config_array;
@@ -69,13 +83,15 @@ struct frustmag_config_policy
         for (double & elem : v) {
             elem = 0;
             for (auto const& cell : lat.cells()) {
-                double prod = 1;
-                for (size_t a : ind) {
-                    prod *= cell[block(a)][component(a)];
+                for (size_t ic = 0; ic < cell.size() / n_block(); ++ic) {
+                    double prod = 1;
+                    for (size_t a : ind) {
+                        prod *= cell[ic * n_block() + block(a)][component(a)];
+                    }
+                    elem += prod;
                 }
-                elem += prod;
             }
-            elem *= *w_it / lat.cells().size();
+            elem *= *w_it / (lat.cells().size() * config_array::n_basis / n_block());
 
             advance_ind(ind);
             ++w_it;
@@ -85,6 +101,7 @@ struct frustmag_config_policy
 
 private:
     using ElementPolicy::block;
+    using ElementPolicy::n_block;
     using ElementPolicy::component;
     using BasePolicy::advance_ind;
     using BasePolicy::weights;
@@ -94,6 +111,7 @@ private:
 inline void define_frustmag_config_policy_parameters(alps::params & parameters) {
     parameters
         .define<bool>("symmetrized", true, "use symmetry <S_x S_y> == <S_y S_x>")
+        .define<std::string>("cluster", "lattice", "cluster used for SVM config")
         .define<size_t>("rank", "rank of the order parameter tensor");
 }
 
@@ -105,22 +123,33 @@ auto frustmag_config_policy_from_parameters(alps::params const& parameters,
 {
 #define CONFPOL_CREATE()                                                \
     return std::unique_ptr<config_policy<Config, Introspector>>(        \
-        new frustmag_config_policy<Config, Introspector, SymmetryPolicy>( \
-            rank, std::move(elempol), unsymmetrize));                   \
+        new frustmag_config_policy<Config, Introspector,                \
+            SymmetryPolicy, decltype(elempol)>(                         \
+                rank, std::move(elempol), unsymmetrize));
+
+#define CONFPOL_BRANCH_SYMM()                                           \
+    if (parameters["symmetrized"].as<bool>()) {                         \
+        using SymmetryPolicy = symmetry_policy::symmetrized;            \
+        CONFPOL_CREATE()                                                \
+    } else {                                                            \
+        using SymmetryPolicy = symmetry_policy::none;                   \
+        CONFPOL_CREATE()                                                \
+    }
 
 
     // set up SVM configuration policy
     size_t rank = parameters["rank"].as<size_t>();
-
-    element_policy::lattice<Config> elempol{};
-    if (parameters["symmetrized"].as<bool>()) {
-        using SymmetryPolicy = symmetry_policy::symmetrized;
-        CONFPOL_CREATE()
+    std::string clname = parameters["cluster"];
+    if (clname == "lattice") {
+        element_policy::lattice<Config> elempol{};
+        CONFPOL_BRANCH_SYMM()
+    } else if (clname == "single") {
+        element_policy::single<Config> elempol{};
+        CONFPOL_BRANCH_SYMM()
     } else {
-        using SymmetryPolicy = symmetry_policy::none;
-        CONFPOL_CREATE()
+        throw std::runtime_error("Invalid cluster spec: " + clname);
     }
 
-
+#undef CONFPOL_BRANCH_SYMM
 #undef CONFPOL_CREATE
 }
